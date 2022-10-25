bd6d5aeea16f6c2de4d83df1f7f652195c353747
Lokesh Shelva <5selva19@gmail.com>
2022-10-25 11:34:54 +0530

    gitignore update

--- CustomAlgo.py
+++ CustomAlgo.py
@@ -1,20 +1,17 @@
-import hashlib
 import json
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 from base64 import b64encode, b64decode
-import hmac
-from hashlib import sha256
-
-from requests import get
 
 class CustomAlgo:
     def __init__(self, user_id, **kwargs):
         self.user_id = user_id
         self.kwargs = kwargs
 
-    def encrypt(self, payload, reciever_id):
+    def encrypt(self, payload: str, reciever_id: int) -> str:
         encryption_key = self.__generate_aes_key(16)
         rsa_key = self.__import_rsa_key()
         rsa_reciever_key = self.__get_reciever_key(reciever_id)
@@ -35,15 +32,15 @@ class CustomAlgo:
         encoded_nonce = self.__base64_encode(nonce)
 
         message = ".".join([x.decode('utf-8') for x in (encoded_header, encoded_encrypted_encryption_key, encoded_nonce, encoded_cipher_text, encoded_auth_tag)])
-        
-        message_digest = hashlib.sha256(message.encode('utf-8')).hexdigest()
-        message_signature = PKCS1_OAEP.new(rsa_key).encrypt(message_digest.encode('utf-8'))
-        encoded_messagae_signature = self.__base64_encode(message_signature)
 
-        return message + "." + encoded_messagae_signature.decode('utf-8')
+        message_hash = SHA256.new(message.encode('utf-8'))
+        message_signature = pkcs1_15.new(rsa_key).sign(message_hash)
+        encoded_message_signature = self.__base64_encode(message_signature)
+
+        return message + "." + encoded_message_signature.decode('utf-8')
 
 
-    def decrypt(self, message):
+    def decrypt(self, message: str) -> str:
         rsa_key = self.__import_rsa_key()
         rsa_sender_key = self.__get_sender_key(1)
 
@@ -51,29 +48,28 @@ class CustomAlgo:
         message = ".".join(split_list[:-1])
         encoded_header, encoded_encrypted_encryption_key, encoded_nonce, encoded_cipher_text, encoded_auth_tag, encoded_message_signature = split_list
         
-        #!TODO: Check with digest
         recieved_message_signature = self.__base64_decode(encoded_message_signature)
-        recieved_message_digest = PKCS1_OAEP.new(rsa_sender_key.public_key()).decrypt(recieved_message_signature)
-        print(recieved_message_digest)
-        calculated_message_digest = hashlib.sha256(message.encode('utf-8')).hexdigest()
-        print(calculated_message_digest)
+        calculated_message_hash = SHA256.new(message.encode('utf-8'))
 
-        if not hmac.compare_digest(recieved_message_digest, calculated_message_digest):
-            print("Digest does not match")
+        try:
+            pkcs1_15.new(rsa_sender_key.public_key()).verify(calculated_message_hash, recieved_message_signature)
+        except:
+            print("Message Signature does not match.")
+            return 
 
         header = json.loads(self.__base64_decode(encoded_header).decode('utf-8'))
         encrypted_encryption_key = self.__base64_decode(encoded_encrypted_encryption_key)
         cipher_text = self.__base64_decode(encoded_cipher_text)
+        auth_tag = self.__base64_decode(encoded_auth_tag)
+        nounce = self.__base64_decode(encoded_nonce)
 
         rsa_encryptor = PKCS1_OAEP.new(rsa_key)
         encryption_key = rsa_encryptor.decrypt(encrypted_encryption_key)
         
-        nounce = self.__base64_decode(encoded_nonce)
-
         aes_encryptor = AES.new(encryption_key, AES.MODE_EAX, nonce=nounce)
-        payload = aes_encryptor.decrypt(cipher_text)
+        payload = aes_encryptor.decrypt_and_verify(cipher_text, auth_tag)
         
-        return payload
+        return payload.decode('utf-8')
         
     def __create_header(self):
         header = {
